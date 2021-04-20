Fix validation log; Add save_frames option; WIP: experiments without output normalization between [0,1]

datasets.py
@@ -424,7 +424,6 @@ class ImagesFromFolderInterpol(data.Dataset):
     self.in_imgs = []
     self.ref_imgs = []
     def parseTrainData(path):
-        path = path
         images = sorted( glob( join(path, '*.' + iext) ) )
         for i in range(0,len(images)-2, 2):
             im1 = images[i]
@@ -432,15 +431,18 @@ class ImagesFromFolderInterpol(data.Dataset):
             im2 = images[i+2]
             self.in_imgs += [ [ im1, im2 ] ]
             self.ref_imgs += [ [ ref ] ]
-    # import pdb; pdb.set_trace()
     if scanSubdir:
         print(f"[WARNING]: assuming that all samples have the same or higher resolution than {self.crop_size}")
         subdir_paths = [f.path for f in os.scandir(root) if f.is_dir()]
         for subdir in subdir_paths:
             parseTrainData(subdir)
     elif annotation_file != '': #Vimeo90k
-        print(f"[WARNING] Loading Vimeo90k from .txt description")
+        print(f"[LOG] Loading Vimeo90k from .txt description")
         subdir_paths = [f"{root}/{x.strip()}/" for x in open(annotation_file)]
+        # if "val" in annotation_file:   
+        #     subdir_paths = subdir_paths[:100]
+        # else:
+        #     subdir_paths = subdir_paths[:500]
         for subdir in subdir_paths:
             parseTrainData(subdir)
     else:
@@ -476,7 +478,7 @@ class ImagesFromFolderInterpol(data.Dataset):
     in_images = np.array(in_images).transpose(3,0,1,2)
     in_images = torch.from_numpy(in_images.astype(np.float32))
     ref_img = cropper(ref_img)
-    ref_img = (np.array(ref_img).transpose(2,0,1)) #/self.rgb_max
+    ref_img = (np.array(ref_img).transpose(2,0,1))#/self.rgb_max
     ref_img = torch.from_numpy(ref_img.astype(np.float32))
 
     return [in_images], [ref_img]

losses.py
@@ -93,6 +93,6 @@ class MSSSIMLoss(nn.Module):
         self.loss_labels = ['MS-SSIM']
 
     def forward(self, output, target):
-        lossvalue = 100*(1 - ms_ssim(output, target, data_range=1.0, size_average=True))
+        lossvalue = 100*(1 - ms_ssim(output, target, data_range=255.0, size_average=True))
         
         return [ lossvalue ]

main.py
@@ -51,6 +51,7 @@ if __name__ == '__main__':
     parser.add_argument('--inference_batch_size', type=int, default=1)
     parser.add_argument('--inference_n_batches', type=int, default=-1)
     parser.add_argument('--save_flow', action='store_true', help='save predicted flows to file')
+    parser.add_argument('--save_frames', action='store_true', help='save predicted frames to file')
 
     parser.add_argument('--resume', default='', type=str, metavar='PATH', help='path to latest checkpoint (default: none)')
     parser.add_argument('--log_frequency', '--summ_iter', type=int, default=1, help="Log every n batches")
@@ -248,13 +249,13 @@ if __name__ == '__main__':
     def train(args, epoch, start_iteration, data_loader, model, optimizer, logger, is_validate=False, offset=0):
         statistics = []
         total_loss = 0
-
         if is_validate:
             model.eval()
             title = 'Validating Epoch {}'.format(epoch)
             args.validation_n_batches = np.inf if args.validation_n_batches < 0 else args.validation_n_batches
             progress = tqdm(tools.IteratorTimer(data_loader), ncols=100, total=np.minimum(len(data_loader), args.validation_n_batches), leave=True, position=offset, desc=title)
         else:
+            # import pdb; pdb.set_trace()
             model.train()
             title = 'Training Epoch {}'.format(epoch)
             args.train_n_batches = np.inf if args.train_n_batches < 0 else args.train_n_batches
@@ -314,8 +315,10 @@ if __name__ == '__main__':
 
             progress.set_description(title + ' ' + tools.format_dictionary_of_losses(loss_labels, statistics[-1]))
 
-            if ((((global_iteration + 1) % args.log_frequency) == 0 and not is_validate) or
-                (is_validate and batch_idx == args.validation_n_batches - 1)):
+            if ((((global_iteration + 1) % args.log_frequency) == 0)):
+            
+            # if ((((global_iteration + 1) % args.log_frequency) == 0 and not is_validate) or
+            #     (is_validate and batch_idx == args.validation_n_batches - 1)):
 
                 global_iteration = global_iteration if not is_validate else start_iteration
 
@@ -350,7 +353,10 @@ if __name__ == '__main__':
             flow_folder = "{}/inference/{}.epoch-{}-flow-field".format(args.save,args.name.replace('/', '.'),epoch)
             if not os.path.exists(flow_folder):
                 os.makedirs(flow_folder)
-        
+        if args.save_frames:
+            frame_folder = "{}/inference/{}.epoch-{}-interpol-frames".format(args.save,args.name.replace('/', '.'),epoch)
+            if not os.path.exists(frame_folder):
+                os.makedirs(frame_folder)
         # visualization folder
         if args.inference_visualize:
             flow_vis_folder = "{}/inference/{}.epoch-{}-flow-vis".format(args.save, args.name.replace('/', '.'), epoch)
@@ -395,7 +401,15 @@ if __name__ == '__main__':
                         flow_utils.visulize_flow_file(
                             join(flow_folder, '%06d.flo' % (batch_idx * args.inference_batch_size + i)),flow_vis_folder)
                    
-                            
+            if args.save_frames:
+                # import pdb; pdb.set_trace()
+                for i in range(args.inference_batch_size):
+                    _pframe = output[i].data.cpu().numpy().transpose(1, 2, 0)
+                    # _pframe = (_pframe*255).astype(int).clip(min=0,max=255)
+                    _pframe = (_pframe).astype(int).clip(min=0,max=255)
+                    import scipy.misc
+                    f_idx = (batch_idx * args.inference_batch_size + i)
+                    scipy.misc.toimage(_pframe, cmin=0, cmax=255).save(f'{frame_folder}/{f_idx:06d}.png')
             progress.set_description('Inference Averages for Epoch {}: '.format(epoch) + tools.format_dictionary_of_losses(loss_labels, np.array(statistics).mean(axis=0)))
             progress.update(1)
 

models.py
@@ -524,6 +524,73 @@ class FlowNet2CSS(nn.Module):
 #     def fowrward(self, x):
 #         return self.flownet(x)
 
+class BasicResBlock(nn.Module):
+    def __init__(self, in_featuremaps, k_number=128, k_size=3):
+        super(BasicResBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_featuremaps, k_number, kernel_size=k_size, stride=1,padding=1)
+        self.bn1 = nn.BatchNorm2d(k_number)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(k_number, k_number, kernel_size=k_size, stride=1,padding=1)
+        self.bn2 = nn.BatchNorm2d(k_number)
+        self.stride = 1
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        out += identity
+
+        return out
+
+
+class DummyModel(nn.Module):
+    def __init__(self, args):
+        super(DummyModel,self).__init__()
+        self.args = args
+        
+        res_kernel_number = 8
+        self.convResIn_img1 = nn.Conv2d(3, res_kernel_number, kernel_size=3, stride=1, padding=1)
+        self.resBlock_img1 = BasicResBlock(res_kernel_number, k_number=res_kernel_number)
+        self.convResOut_img1 = nn.Conv2d(res_kernel_number, 3, kernel_size=3, stride=1, padding=1)
+        
+        self.convResIn_img2 = nn.Conv2d(3, res_kernel_number, kernel_size=3, stride=1, padding=1)
+        self.resBlock_img2 = BasicResBlock(res_kernel_number, k_number=res_kernel_number)
+        self.convResOut_img2 = nn.Conv2d(res_kernel_number, 3, kernel_size=3, stride=1, padding=1)
+        
+        self.convResIn_final = nn.Conv2d(6, res_kernel_number, kernel_size=3, stride=1, padding=1)
+        self.resBlock_final = BasicResBlock(res_kernel_number, k_number=res_kernel_number)
+        self.convResOut_final = nn.Conv2d(res_kernel_number, 3, kernel_size=3, stride=1, padding=1)
+
+        self.rgb_max = 255
+    def forward(self, inputs):
+        # Same as fnet2 input
+        rgb_mean = inputs.contiguous().view(inputs.size()[:2]+(-1,)).mean(dim=-1).view(inputs.size()[:2] + (1,1,1,))
+        
+        x = (inputs - rgb_mean) / self.rgb_max
+        x1 = x[:,:,0,:,:]
+        x1 = self.convResIn_img1(x1)
+        x1 = self.resBlock_img1(x1)
+        x1 = self.convResOut_img1(x1)
+        
+        x2 = x[:,:,1,:,:]
+        x2 = self.convResIn_img2(x2)
+        x2 = self.resBlock_img2(x2)
+        x2 = self.convResOut_img2(x2)
+
+        
+        x = torch.cat((x1,x2), dim = 1)
+
+        prediction = self.convResIn_final(x)
+        prediction = self.resBlock_final(prediction)
+        prediction = self.convResOut_final(prediction)
+        return prediction
+
 class InterpolNet(nn.Module):
     def __init__(self, args):
         super(InterpolNet,self).__init__()
@@ -545,10 +612,26 @@ class InterpolNet(nn.Module):
         else:
             self.resample1 = Resample2d()
         
-        self.conv1 = nn.Conv2d(9, 3, kernel_size=5, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(3, 3, kernel_size=3, stride=1, padding=1)
-        self.conv3 = nn.Conv2d(3, 3, kernel_size=1, stride=1, padding=1)
-        self.relu = nn.ReLU()
+        res_kernel_number = 128
+        self.convResIn_img1 = nn.Conv2d(3, res_kernel_number, kernel_size=3, stride=1, padding=1)
+        self.resBlock_img1 = BasicResBlock(res_kernel_number, k_number=res_kernel_number)
+        self.convResOut_img1 = nn.Conv2d(res_kernel_number, 3, kernel_size=3, stride=1, padding=1)
+        
+        self.convResIn_img2 = nn.Conv2d(3, res_kernel_number, kernel_size=3, stride=1, padding=1)
+        self.resBlock_img2 = BasicResBlock(res_kernel_number, k_number=res_kernel_number)
+        self.convResOut_img2 = nn.Conv2d(res_kernel_number, 3, kernel_size=3, stride=1, padding=1)
+        
+        self.convResIn_mid = nn.Conv2d(3, res_kernel_number, kernel_size=3, stride=1, padding=1)
+        self.resBlock_mid = BasicResBlock(res_kernel_number, k_number=res_kernel_number)
+        self.convResOut_mid = nn.Conv2d(res_kernel_number, 3, kernel_size=3, stride=1, padding=1)
+
+        self.convResIn_final = nn.Conv2d(9, res_kernel_number, kernel_size=3, stride=1, padding=1)
+        self.resBlock_final = BasicResBlock(res_kernel_number, k_number=res_kernel_number)
+        self.convResOut_final = nn.Conv2d(res_kernel_number, 3, kernel_size=3, stride=1, padding=1)
+        # self.conv1 = nn.Conv2d(9, 3, kernel_size=5, stride=1, padding=1)
+        # self.conv2 = nn.Conv2d(3, 3, kernel_size=3, stride=1, padding=1)
+        # self.conv3 = nn.Conv2d(3, 3, kernel_size=1, stride=1, padding=1)
+        # self.relu = nn.ReLU()
 
         self.rgb_max = 255
     def forward(self, inputs):
@@ -557,19 +640,30 @@ class InterpolNet(nn.Module):
         
         x = (inputs - rgb_mean) / self.rgb_max
         x1 = x[:,:,0,:,:]
+        x1 = self.convResIn_img1(x1)
+        x1 = self.resBlock_img1(x1)
+        x1 = self.convResOut_img1(x1)
+        
         x2 = x[:,:,1,:,:]
+        x2 = self.convResIn_img2(x2)
+        x2 = self.resBlock_img2(x2)
+        x2 = self.convResOut_img2(x2)
+
+        
         x = torch.cat((x1,x2), dim = 1)
 
-       
-        # diff_img0 = x[:,:3,:,:] - resampled_img1 
-
         flow = self.flownet(inputs)
-         # warp img1 to img0; magnitude of diff between img0 and and warped_img1, 
-        resampled_img1 = self.resample1(x[:,3:,:,:], flow)
+        warped_mid = self.resample1(x[:,3:,:,:], flow)
+
+        warped_mid = self.convResIn_mid(warped_mid)
+        warped_mid = self.resBlock_mid(warped_mid)
+        warped_mid = self.convResOut_mid(warped_mid)
         
-        interpol_input = torch.cat((x1,x2, resampled_img1), dim = 1)
-        prediction = self.conv1(interpol_input)
-        prediction = self.conv2(prediction)
-        prediction = self.conv3(prediction)
-        prediction = self.relu(prediction)
+        interpol_input = torch.cat((x1,x2, warped_mid), dim = 1)
+
+
+        prediction = self.convResIn_final(interpol_input)
+        prediction = self.resBlock_final(prediction)
+        prediction = self.convResOut_final(prediction)
+        # prediction = self.relu(prediction)
         return prediction